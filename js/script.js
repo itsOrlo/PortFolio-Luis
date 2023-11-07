d3246a86d8754d5549a419a35137fe4c22f004c5
itsOrlo <orlo.montesdeoca@gmail.com>
2023-11-06 22:53:04 -0500

    Implementación JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// navbar.js
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.querySelector("#navbarSupportedContent");
+
+  navbarToggler.addEventListener("click", function () {
+    navbarCollapse.classList.toggle("show");
+  });
+});
